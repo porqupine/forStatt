28f9d181a91a8a44568295c2342bd1af9b2d140a
Stoytcho Stoytchev <sstoytchev@gmail.com>
2021-03-24 01:18:51 -0700

    added comments

--- binary-search-tree/binary-search-tree.js
+++ binary-search-tree/binary-search-tree.js
@@ -1,7 +1,10 @@
 //
-// This is only a SKELETON file for the 'Binary Search Tree' exercise. It's been provided as a
-// convenience to get you started writing code faster.
-//
+//Please note I had to change the test file to actually run
+//The original skeleton did not include an "each" method.
+//There is jquery each for traversing the dom tree, or forEach on a
+//js array, but neither of those made sense. The problem spec
+//did not say the BST had to be in the form of an array. In fact
+//the setup indicated that it expected nodes.
 
 export class BinarySearchTree {
   constructor(value) {
@@ -11,34 +14,52 @@ export class BinarySearchTree {
   }
   insert(value) {
     if (value <= this.data) {
-      this.left = this.insertHelper(value, this.left);
+      this.insertHelper(value, "left");
     } else {
-      this.right = this.insertHelper(value, this.right);
+      this.insertHelper(value, "right");
     }
   }
+  //Not strictly required but it makes reading insert easier
+  //Using this[node] isn't as readable as accessing this.left/this.right
+  //directly, but it simplifies the code a bit.
   insertHelper(value, node) {
-    if (node) {
-      node.insert(value);
+    let child = this[node];
+    if (child) {
+      child.insert(value);
     } else {
-      node = new BinarySearchTree(value);
+      this[node] = new BinarySearchTree(value);
     }
-    return node;
   }
-  getSorted(){
-    let output = []
-    let stack = [this]
-    let current = this.left
-    while(stack.length || current !== null) {
-      if(current == null){
-        current = stack.pop()
-        output.push(current.data)
-        current = current.right
-      }
-      else{
-        stack.push(current)
-        current = current.left
+  //I wrote this method to return the sorted array that the tests seem to expect.
+  //Since I did insert recursively, I opted to go that route here as well.
+  getSorted() {
+    let output = [];
+    if (this.left) {
+      output = output.concat(this.left.getSorted());
+    }
+    output.push(this.data);
+    if (this.right) {
+      output = output.concat(this.right.getSorted());
+    }
+    return output;
+  }
+  //Iterative version for comparison
+  // - seems to run ~0.05 s faster on average for the test suite, maybe.
+  // There isn't a really consistent difference, even at ~5k nodes
+  getSortedIterative() {
+    let output = [];
+    let stack = [this];
+    let current = this.left;
+    while (stack.length || current !== null) {
+      if (current == null) {
+        current = stack.pop();
+        output.push(current.data);
+        current = current.right;
+      } else {
+        stack.push(current);
+        current = current.left;
       }
     }
-    return output
+    return output;
   }
 }
--- secret-handshake/secret-handshake.js
+++ secret-handshake/secret-handshake.js
@@ -3,7 +3,8 @@
 //where the keys are the binary integers to allow bitmasking
 //There are a number of different ways to achieve the same result
 //including converting the input to a binary string, or
-//bitshifting the input to the right repeatedly and using the mask of 1
+//bitshifting the input to the right repeatedly and using a mask of 1
+
 const codes = {
   1: "wink",
   2: "double blink",
@@ -14,8 +15,16 @@ const reverse = 16;
 
 export const commands = (code) => {
   let output = [];
+  //Negative numbers are stored in 2's compliment
+  // so this method wouldn't work without modification.
+  //Additionally there are none in the test spec so I assume they're not required.
+  //Shorting the logic on 0 helps us avoid some computation.
   if (code <= 0) return output;
   let keys = Object.keys(codes);
+  //There are a few different ways to get this reversal, I chose to
+  //reverse the list of keys. This is O(n) but n is tiny.
+  //You can avoid this by using a for loop and setting the start/end points
+  //based on reverse - [0 to keys.length) normally and (keys.length to 0] for reverse
   if (code & reverse) {
     keys = keys.reverse();
   }
